5720eea6128520b92d67a37157f247d760ec986b
Anton Krasovki <antohkad21@gmail.com>
2025-08-09 13:01:07 +0300

    add: new game calculate numbers

---
+++ bin/brain-calc.js
@@ -0,0 +1,66 @@
+import readlineSync from "readline-sync";
+import {
+  showGameGreeting,
+  getNameUser,
+  getRandomNumber,
+  isCorrectAnswer,
+} from "./utils.js";
+
+const symbolsArr = ["+", "-", "*"];
+
+const getSymbol = (symbols) => {
+  return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+const calculateNumbers = (firstNumber, secondNumber, symbol) => {
+  let result = 0;
+  if (symbol === "+") {
+    result = firstNumber + secondNumber;
+  }
+  if (symbol === "-") {
+    result = firstNumber - secondNumber;
+  }
+  if (symbol === "*") {
+    result = firstNumber * secondNumber;
+  }
+  return result;
+};
+
+const getAnswer = (firstNumber, secondNumber, symbol) => {
+  console.log(`Question: ${firstNumber} ${symbol} ${secondNumber}`);
+  const answer = readlineSync.question("Your answer: ");
+  return Number(answer);
+};
+
+const startGameCalculate = () => {
+  const startRange = 1;
+  const endRange = 100;
+  let flag = true;
+
+  for (let i = 0; i < 3; i += 1) {
+    const firstNumber = getRandomNumber(startRange, endRange);
+    const secondNumber = getRandomNumber(startRange, endRange);
+    const symbol = getSymbol(symbolsArr);
+    const answerRight = calculateNumbers(firstNumber, secondNumber, symbol);
+    const userAnswer = getAnswer(firstNumber, secondNumber, symbol);
+    const correctAnswer = isCorrectAnswer(answerRight, userAnswer);
+
+    if (correctAnswer) {
+      console.log("Correct!");
+    } else {
+      flag = false;
+      break;
+    }
+  }
+
+  return flag;
+};
+
+showGameGreeting();
+const nameUser = getNameUser();
+const resultGame = startGameCalculate();
+if (resultGame) {
+  console.log(`Congratulations, ${nameUser}!`);
+} else {
+  console.log(`Let's try again, ${nameUser}!`);
+}
