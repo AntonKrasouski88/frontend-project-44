8b27a37b8e7162acaf239416e560e3b705f074c6
Anton Krasovki <antohkad21@gmail.com>
2025-08-10 13:53:03 +0300

    Add a gafeat(game): implement core logic for NOD game in separate fileme

---
+++ bin/brain-gcd.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+import readlineSync from "readline-sync";
+import {
+  showGameGreeting,
+  getNameUser,
+  getRandomNumber,
+  isCorrectAnswer,
+} from "./utils.js";
+
+const getListDivisors = (num) => {
+  const arrDivisors = [num];
+  for (let i = 1; i < Math.ceil(num / 2); i += 1) {
+    num % i === 0 && arrDivisors.push(i);
+  }
+  return arrDivisors;
+};
+
+const getMinDivisors = (arrDivisors1, arrDivisors2) => {
+  let divisors = arrDivisors1.reduce((acc, divisor) => {
+    if (arrDivisors2.includes(divisor) && divisor !== 1) {
+      acc.push(divisor);
+    }
+    return acc;
+  }, []);
+  if (divisors.length !== 0) {
+    return divisors[0];
+  }
+
+  return 1;
+};
+
+const getAnswerUser = (num1, num2) => {
+  console.log("Find the greatest common divisor of given numbers.");
+  console.log(`Question: ${num1} ${num2}`);
+  let answer;
+  do {
+    answer = readlineSync.question("Your answer: ");
+    if (answer.length === 0 || Number.isNaN(Number(answer))) {
+      console.log("Please answer with a number");
+    }
+  } while (answer.length === 0 || Number.isNaN(Number(answer)));
+  return Number(answer);
+};
+
+const startGameNod = () => {
+  const startRange = 1;
+  const endRange = 100;
+  let flag = true;
+
+  for (let i = 0; i < 3; i += 1) {
+    const firstNumber = getRandomNumber(startRange, endRange);
+    const secondNumber = getRandomNumber(startRange, endRange);
+    const listDivisorsFirstNum = getListDivisors(firstNumber);
+    console.log(listDivisorsFirstNum);
+
+    const listDivisorsSecondNum = getListDivisors(secondNumber);
+    const minDivisor = getMinDivisors(
+      listDivisorsFirstNum,
+      listDivisorsSecondNum
+    );
+    const userAnswer = getAnswerUser(firstNumber, secondNumber);
+    const correctAnswer = isCorrectAnswer(userAnswer, minDivisor);
+    if (correctAnswer) {
+      console.log("Correct!");
+    } else {
+      flag = false;
+      break;
+    }
+  }
+
+  return flag;
+};
+
+showGameGreeting();
+const nameUser = getNameUser();
+const resultGame = startGameNod();
+if (resultGame) {
+  console.log(`Congratulations, ${nameUser}!`);
+} else {
+  console.log(`Let's try again, ${nameUser}!`);
+}
