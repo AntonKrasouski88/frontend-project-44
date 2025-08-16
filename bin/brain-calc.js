c43061d6eae3c223cd927b70e16cb4b7f7392464
Anton Krasovki <antohkad21@gmail.com>
2025-08-16 10:23:49 +0300

    feat(game-even): extract brain-games logic into separate module

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,71 +1,13 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync'
 import {
   showGameGreeting,
   getNameUser,
-  getRandomNumber,
-  isCorrectAnswer,
+  showResultGame,
 } from '../src/games/utils.js'
 
-const symbolsArr = ['+', '-', '*']
-
-const getSymbol = symbols => symbols[Math.floor(Math.random() * symbols.length)]
-
-const calculateNumbers = (firstNumber, secondNumber, symbol) => {
-  let result = 0
-  if (symbol === '+') {
-    result = firstNumber + secondNumber
-  }
-  if (symbol === '-') {
-    result = firstNumber - secondNumber
-  }
-  if (symbol === '*') {
-    result = firstNumber * secondNumber
-  }
-  return result
-}
-
-const getAnswer = (firstNumber, secondNumber, symbol) => {
-  console.log('What is the result of the expression?')
-  console.log(`Question: ${firstNumber} ${symbol} ${secondNumber}`)
-  const answer = readlineSync.question('Your answer: ')
-  return Number(answer)
-}
-
-const startGameCalculate = () => {
-  const startRange = 1
-  const endRange = 100
-  let flag = true
-
-  for (let i = 0; i < 3; i += 1) {
-    const firstNumber = getRandomNumber(startRange, endRange)
-    const secondNumber = getRandomNumber(startRange, endRange)
-    const symbol = getSymbol(symbolsArr)
-    const answerRight = calculateNumbers(firstNumber, secondNumber, symbol)
-    const userAnswer = getAnswer(firstNumber, secondNumber, symbol)
-    const correctAnswer = isCorrectAnswer(answerRight, userAnswer)
-
-    if (correctAnswer) {
-      console.log('Correct!')
-    }
-    else {
-      console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${answerRight}'.`,
-      )
-      flag = false
-      break
-    }
-  }
-
-  return flag
-}
+import startGameCalculate from '../src/games/game-calc.js'
 
 showGameGreeting()
 const nameUser = getNameUser()
 const resultGame = startGameCalculate()
-if (resultGame) {
-  console.log(`Congratulations, ${nameUser}!`)
-}
-else {
-  console.log(`Let's try again, ${nameUser}!`)
-}
+showResultGame(resultGame, nameUser)
---
+++ src/games/game-calc.js
@@ -0,0 +1,57 @@
+import readlineSync from 'readline-sync'
+import { getRandomNumber, isCorrectAnswer } from './utils.js'
+
+const symbolsArr = ['+', '-', '*']
+
+const getSymbol = symbols => symbols[Math.floor(Math.random() * symbols.length)]
+
+const calculateNumbers = (firstNumber, secondNumber, symbol) => {
+  let result = 0
+  if (symbol === '+') {
+    result = firstNumber + secondNumber
+  }
+  if (symbol === '-') {
+    result = firstNumber - secondNumber
+  }
+  if (symbol === '*') {
+    result = firstNumber * secondNumber
+  }
+  return result
+}
+
+const getAnswer = (firstNumber, secondNumber, symbol) => {
+  console.log('What is the result of the expression?')
+  console.log(`Question: ${firstNumber} ${symbol} ${secondNumber}`)
+  const answer = readlineSync.question('Your answer: ')
+  return Number(answer)
+}
+
+const startGameCalculate = () => {
+  const startRange = 1
+  const endRange = 100
+  let flag = true
+
+  for (let i = 0; i < 3; i += 1) {
+    const firstNumber = getRandomNumber(startRange, endRange)
+    const secondNumber = getRandomNumber(startRange, endRange)
+    const symbol = getSymbol(symbolsArr)
+    const answerRight = calculateNumbers(firstNumber, secondNumber, symbol)
+    const userAnswer = getAnswer(firstNumber, secondNumber, symbol)
+    const correctAnswer = isCorrectAnswer(answerRight, userAnswer)
+
+    if (correctAnswer) {
+      console.log('Correct!')
+    }
+    else {
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${answerRight}'.`,
+      )
+      flag = false
+      break
+    }
+  }
+
+  return flag
+}
+
+export default startGameCalculate
