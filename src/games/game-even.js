6c2f9d516c4d7c6fca0abe3caa4cc4098cdf8332
AntonKrasovki88 <antohkad21@gmail.com>
2025-11-02 21:24:03 +0300

    Fix function

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -5,28 +5,10 @@ export const gameEven = () => {
   const startRange = 1
   const endRange = 100
   const randomNumber = getRandomNumber(startRange, endRange)
-  const isEvenNumber = (num) => {
-    if (num < 2) {
-      return 'no'
-    }
-    if (num === 2) {
-      return 'yes'
-    }
-    if (num % 2 === 0) {
-      return 'no'
-    }
-    const limit = Math.ceil(Math.sqrt(num))
-    for (let i = 3; i <= limit; i += 2) {
-      if (num % i === 0) {
-        return 'no'
-      }
-    }
-    return 'yes'
-  }
-  const rightAnswer = isEvenNumber(randomNumber)
   console.log('Answer "yes" if the number is even, otherwise answer "no".')
   console.log(`Question: ${randomNumber}`)
   const answerUser = getAnswerUserYesNo()
+  const rightAnswer = randomNumber % 2 === 0 ? 'yes' : 'no'
   showRoundResult(answerUser, rightAnswer)
   return answerUser === rightAnswer
 }
--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -4,15 +4,32 @@ import {
   showRoundResult,
 } from './utils.js'
 
-export const brain_prime = () => {
+export const brainPrime = () => {
   const startRange = 1
   const endRange = 100
   const number = getRandomNumber(startRange, endRange)
-
+  const isPrimeNumber = (num) => {
+    if (num < 2) {
+      return 'no'
+    }
+    if (num === 2) {
+      return 'yes'
+    }
+    if (num % 2 === 0) {
+      return 'no'
+    }
+    const limit = Math.ceil(Math.sqrt(num))
+    for (let i = 3; i <= limit; i += 2) {
+      if (num % i === 0) {
+        return 'no'
+      }
+    }
+    return 'yes'
+  }
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".')
   console.log(`Question: ${number}`)
   const answerUser = getAnswerUserYesNo()
-  const rightAnswer = number % 2 === 0 ? 'yes' : 'no'
+  const rightAnswer = isPrimeNumber(number)
   showRoundResult(answerUser, rightAnswer)
   return answerUser === rightAnswer
 }
